368e97e9a5233d424d1ce577bbdae96a89b73253
Mael Rimbault <mael.rimbault@adjust.com>
2020-07-16 18:31:30 +0200

    Add downgrade script

--- sql/first_last_agg--0.1.5--0.1.4.sql
+++ sql/first_last_agg--0.1.5--0.1.4.sql
@@ -1,28 +1,24 @@
 /*
- * Make aggregate functions parallel safe, starting with PG10.
+ * Downgrade and create functions and aggregates without parallel safe.
  */
 DO $$
 DECLARE version_num integer;
 BEGIN
   SELECT current_setting('server_version_num') INTO STRICT version_num;
   IF version_num > 90600 THEN
-    EXECUTE $E$ ALTER FUNCTION last_sfunc(anyelement, anyelement) PARALLEL SAFE   $E$;
-    EXECUTE $E$ ALTER FUNCTION first_sfunc(anyelement, anyelement) PARALLEL SAFE   $E$;
+    EXECUTE $E$ ALTER FUNCTION last_sfunc(anyelement, anyelement) PARALLEL UNSAFE   $E$;
+    EXECUTE $E$ ALTER FUNCTION first_sfunc(anyelement, anyelement) PARALLEL UNSAFE   $E$;
 
     EXECUTE $E$ DROP AGGREGATE IF EXISTS first(anyelement) $E$;
     EXECUTE $E$ CREATE AGGREGATE first(anyelement) (
         SFUNC = first_sfunc,
-        STYPE = anyelement,
-        COMBINEFUNC = first_sfunc,
-        parallel = SAFE
+        STYPE = anyelement
     ); $E$;
 
     EXECUTE $E$ DROP AGGREGATE IF EXISTS last(anyelement) $E$;
     EXECUTE $E$ CREATE AGGREGATE last(anyelement) (
         SFUNC = last_sfunc,
-        STYPE = anyelement,
-        COMBINEFUNC = last_sfunc,
-        parallel = SAFE
+        STYPE = anyelement
     ); $E$;
   END IF;
 END;
